fa6a00697218c15986d188f3e17ace6d9cc43ba0
Robin Boutros <niuage@gmail.com>
2020-05-17 07:55:34 +0200

    first commit

---
+++ GeoGrass.shader
@@ -0,0 +1,80 @@
+﻿// @Cyanilux
+// Grass Geometry Shader, Written for Universal RP with help from https://roystan.net/articles/grass-shader.html
+// Note, doesn't include Lighting or Tessellation
+Shader "Unlit/GeoGrass" {
+		Properties {
+			_Color("Colour", Color) = (1,1,1,1)
+			_Color2("Colour2", Color) = (1,1,1,1)
+			_Width("Width", Float) = 1
+			_RandomWidth("Random Width", Float) = 1
+			_Height("Height", Float) = 1
+			_RandomHeight("Random Height", Float) = 1
+			_WindStrength("Wind Strength", Float) = 0.1
+			[Space]
+		     // Added by niuage
+			_TessellationUniform("Tessellation Uniform", Range(1, 64)) = 1
+		}
+
+		SubShader {
+			Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline" }
+			LOD 300
+
+			Cull Off
+
+			Pass {
+				Name "ForwardLit"
+				Tags {"LightMode" = "UniversalForward"}
+
+				HLSLPROGRAM
+				// Required to compile gles 2.0 with standard srp library
+				#pragma prefer_hlslcc gles
+				#pragma exclude_renderers d3d11_9x gles
+				#pragma target 4.5
+
+				#pragma require geometry
+
+				#pragma vertex vert
+				#pragma geometry geom
+				#pragma fragment frag
+
+                // added by niuage
+				#pragma hull hull
+				#pragma domain domain
+
+				#pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+				#pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+				#pragma multi_compile _ _SHADOWS_SOFT
+
+				// Defines
+
+				#define BLADE_SEGMENTS 1
+
+				// Includes
+
+				#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+				#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+				#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl"
+				#include "grass.hlsl"
+
+				// added by niuage
+				#include "tesselation/CustomTessellation.hlsl"
+
+				// Fragment
+
+				float4 frag(GeometryOutput input) : SV_Target {
+					#if SHADOWS_SCREEN
+						float4 clipPos = TransformWorldToHClip(input.positionWS);
+						float4 shadowCoord = ComputeScreenPos(clipPos);
+					#else
+						float4 shadowCoord = TransformWorldToShadowCoord(input.positionWS);
+					#endif
+
+					Light mainLight = GetMainLight(shadowCoord);
+
+					return lerp(_Color, _Color2, input.uv.y) * mainLight.shadowAttenuation;
+				}
+
+				ENDHLSL
+			}
+		}
+}
---
+++ tesselation/CustomTessellation.hlsl
@@ -0,0 +1,91 @@
+// Tessellation programs based on this article by Catlike Coding:
+// https://catlikecoding.com/unity/tutorials/advanced-rendering/tessellation/
+
+// added by niuage
+#if defined(SHADER_API_D3D11) || defined(SHADER_API_GLES3) || defined(SHADER_API_GLCORE) || defined(SHADER_API_VULKAN) || defined(SHADER_API_METAL) || defined(SHADER_API_PSSL)
+#define UNITY_CAN_COMPILE_TESSELLATION 1
+#   define UNITY_domain                 domain
+#   define UNITY_partitioning           partitioning
+#   define UNITY_outputtopology         outputtopology
+#   define UNITY_patchconstantfunc      patchconstantfunc
+#   define UNITY_outputcontrolpoints    outputcontrolpoints
+#endif
+
+struct vertexInput
+{
+	float4 vertex : POSITION;
+	float3 normal : NORMAL;
+	float4 tangent : TANGENT;
+};
+
+struct vertexOutput
+{
+	float4 vertex : SV_POSITION;
+	float3 normal : NORMAL;
+	float4 tangent : TANGENT;
+};
+
+struct TessellationFactors 
+{
+	float edge[3] : SV_TessFactor;
+	float inside : SV_InsideTessFactor;
+};
+
+vertexInput vert(vertexInput v)
+{
+	return v;
+}
+
+vertexOutput tessVert(vertexInput v)
+{
+	vertexOutput o;
+	// Note that the vertex is NOT transformed to clip
+	// space here; this is done in the grass geometry shader.
+	o.vertex = v.vertex;
+	o.normal = v.normal;
+	o.tangent = v.tangent;
+	return o;
+}
+
+
+// added by niuage - do I need CBUFFER_START here, same as in grass.hlsl?
+CBUFFER_START(UnityPerMaterial)
+float _TessellationUniform;
+CBUFFER_END
+
+TessellationFactors patchConstantFunction (InputPatch<vertexInput, 3> patch)
+{
+	TessellationFactors f;
+	f.edge[0] = _TessellationUniform;
+	f.edge[1] = _TessellationUniform;
+	f.edge[2] = _TessellationUniform;
+	f.inside = _TessellationUniform;
+	return f;
+}
+
+[UNITY_domain("tri")]
+[UNITY_outputcontrolpoints(3)]
+[UNITY_outputtopology("triangle_cw")]
+[UNITY_partitioning("integer")]
+[UNITY_patchconstantfunc("patchConstantFunction")]
+vertexInput hull (InputPatch<vertexInput, 3> patch, uint id : SV_OutputControlPointID)
+{
+	return patch[id];
+}
+
+[UNITY_domain("tri")]
+vertexOutput domain(TessellationFactors factors, OutputPatch<vertexInput, 3> patch, float3 barycentricCoordinates : SV_DomainLocation)
+{
+	vertexInput v;
+
+	#define MY_DOMAIN_PROGRAM_INTERPOLATE(fieldName) v.fieldName = \
+		patch[0].fieldName * barycentricCoordinates.x + \
+		patch[1].fieldName * barycentricCoordinates.y + \
+		patch[2].fieldName * barycentricCoordinates.z;
+
+	MY_DOMAIN_PROGRAM_INTERPOLATE(vertex)
+	MY_DOMAIN_PROGRAM_INTERPOLATE(normal)
+	MY_DOMAIN_PROGRAM_INTERPOLATE(tangent)
+
+	return tessVert(v);
+}
